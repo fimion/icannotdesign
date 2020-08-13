2e8a077a6e2cd8408641b47e0407c3a215a294ac
Alex Riviere <fimion@gmail.com>
2020-08-13 06:06:34 -0400

    Add open graph data?

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { property:"og:title", content:"I CANNOT DESIGN"},
+      { property:"og:type", content:"website"},
+      { property:"og:url", content:"https://icannot.design"},
+      { property:"og:image", content:"https://icannot.design/opengraph.png"},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
