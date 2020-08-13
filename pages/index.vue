187672fd92d86bab292ab63d936c12ae4a229044
Gabor Javorszky <gabor.javorszky@typeform.com>
2020-08-13 10:34:39 +0100

    Add marquee and another gif

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,19 @@
   <div class="main">
     <h1 class="title">I Cannot Design.</h1>
     <hr>
+    <marquee direction="down" width="250" height="200" behavior="alternate">
+      <marquee behavior="alternate">
+        Under construction!!!1!1one
+      </marquee>
+    </marquee>
     <img src="/construction.gif"
          height="36"
          width="343"
          alt="This site is under eternal construction.">
+    <img src="/underconstruction.gif"
+         height="180"
+         width="158"
+         alt="Still under construction">
     <div>
       <img src="/constjackhammer_e0.gif" alt="An animated construction worker operating a jackhammer.">
       <img src="/anim0206-1_e0.gif" alt="A cartoon construction worker eating alternating between eating a sandwich and drinking water.">
@@ -13,7 +22,6 @@
       <img src="/anim0206-1_e0.gif" alt="A cartoon construction worker eating alternating between eating a sandwich and drinking water.">
       <img src="/constjackhammer_e0.gif" alt="An animated construction worker operating a jackhammer.">
     </div>
-
   </div>
 </template>
 
