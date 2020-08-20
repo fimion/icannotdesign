064d0d64193e41260f1f6c322b9e1ee4ea7016a6
Alex <fimion@gmail.com>
2020-08-20 10:15:19 -0400

    make link to home
    - adds link styling in the purple layout
    - adds a link to the default layout to get home

--- layouts/purple.vue
+++ layouts/purple.vue
@@ -22,6 +22,22 @@ export default {
     text-shadow: 0 0 10px turquoise;
   }
 
+  * >>> h1 a, * >>> h1 a:hover, * >>> h1 a:focus, * >>> h1 a:visited,
+  * >>> h2 a, * >>> h2 a:hover, * >>> h2 a:focus, * >>> h2 a:visited,
+  * >>> h3 a, * >>> h3 a:hover, * >>> h3 a:focus, * >>> h3 a:visited,
+  * >>> h4 a, * >>> h4 a:hover, * >>> h4 a:focus, * >>> h4 a:visited,
+  * >>> h5 a, * >>> h5 a:hover, * >>> h5 a:focus, * >>> h5 a:visited,
+  * >>> h6 a, * >>> h6 a:hover, * >>> h6 a:focus, * >>> h6 a:visited
+  {
+    color: inherit;
+  }
+
+  * >>> a, * >>> a:hover, * >>> a:focus, * >>> a:visited {
+    color: forestgreen;
+    text-shadow: 0 0 5px darkorchid;
+  }
+
+
 * >>> hr {
     width: 546px;
     border: none;
